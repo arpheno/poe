6c0942086cc4ef6bbcf478762926554f69d78753
swozny <arpheno@gmail.com>
2024-04-25 18:42:35 +0200

    Splits stash tab api interactor into multiple parts for reusability and testability

--- poe/trade/stash_tabs/caller.py
+++ poe/trade/stash_tabs/caller.py
@@ -9,18 +9,23 @@ from poe.trade.rate_limiter import limit_rate
 
 cache=Redis()
 def _call(tab_index):
-    url = "https://www.pathofexile.com/character-window/get-stash-items"
-    params = dict(accountName="swozn", league=LEAGUE, tabIndex=tab_index, tabs=1)
-    key = "X-Rate-Limit-Account"
-    limits = ['30:60:60','100:1800:600']
-    with limit_rate(key, limits, cache):
-        response = requests.get(url, params=params, headers=headers)
-    pprint(response.headers)
-    response.raise_for_status()
-    data = response.json()
+    data = stash_tab_api_interactor(tab_index)
     tabs = data["tabs"]
     [current_tab] = [tab for tab in tabs if tab["i"] == tab_index]
     current_tab_name = current_tab["n"]
     for item in data["items"]:
         item["stashtab"] = current_tab_name
     return data["items"]
+
+
+def stash_tab_api_interactor(tab_index):
+    url = "https://www.pathofexile.com/character-window/get-stash-items"
+    params = dict(accountName="swozn", league=LEAGUE, tabIndex=tab_index, tabs=1)
+    key = "X-Rate-Limit-Account"
+    limits = ['30:60:60', '100:1800:600']
+    with limit_rate(key, limits, cache):
+        response = requests.get(url, params=params, headers=headers)
+    pprint(response.headers)
+    response.raise_for_status()
+    data = response.json()
+    return data
